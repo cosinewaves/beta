ea07d1e29d4890499a32aab11c13bae87d2e234c
cosinewaves <158834632+cosinewaves@users.noreply.github.com>
2025-05-03 19:15:46 +0100

    Update internalTypings.lua

--- src/beta/internalTypings.lua
+++ src/beta/internalTypings.lua
@@ -1,12 +1,19 @@
+--!strict
+--[[
+	Author: cosinewaves
+	Name: internalTypings.lua
+]]
+
 export type state<T> = {
 	__call: (self: state<T>, newValue: T?, forceUpdate: boolean?) -> T,
 	respond: (self: state<T>, callback: (oldValue: T, newValue: T) -> ()) -> (() -> ()),
 }
 
+
 export type evaluate<T> = {
 	__call: (self: evaluate<T>) -> T,
 	disconnect: (self: evaluate<T>) -> (),
-	value: T,
 }
+
 
 return nil
