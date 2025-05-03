9fed49b5c0417ca3f7a0b5b58d039a26d18a7011
cosinewaves <158834632+cosinewaves@users.noreply.github.com>
2025-05-03 18:19:37 +0100

    Create init.lua

---
+++ src/beta/init.lua
@@ -0,0 +1,11 @@
+local state = require(script.state)
+local internalTypings = require(script.internalTypings)
+local evaluate = require(script.evaluate)
+
+export type state<T> = internalTypings.state<T>
+export type evaluate<T> = internalTypings.evaluate<T>
+
+return table.freeze({
+	state = state,
+	evaluate = evaluate
+})
