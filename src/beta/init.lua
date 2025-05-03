bedfa904875b9d894720ef2a98bbbc32cfa77fad
cosinewaves <158834632+cosinewaves@users.noreply.github.com>
2025-05-03 19:16:23 +0100

    Update init.lua

--- src/beta/init.lua
+++ src/beta/init.lua
@@ -1,11 +1,19 @@
+--!strict
+--[[
+	Author: cosinewaves
+	Name: beta.init.lua
+]]
+
 local state = require(script.state)
 local internalTypings = require(script.internalTypings)
 local evaluate = require(script.evaluate)
+local errors = require(script.errors)
 
 export type state<T> = internalTypings.state<T>
 export type evaluate<T> = internalTypings.evaluate<T>
 
 return table.freeze({
 	state = state,
-	evaluate = evaluate
+	evaluate = evaluate,
+	errors = errors
 })
