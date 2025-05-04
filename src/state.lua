7cea98d10bb25ec733a0ef0dc3a6096bdaf30fd9
cosinewaves <158834632+cosinewaves@users.noreply.github.com>
2025-05-04 10:55:56 +0100

    Update state.lua
    increased documentation

--- src/state.lua
+++ src/state.lua
@@ -14,11 +14,13 @@ function state.new<T>(initial: T): typings.state<T>
 		errors.new("state", "initial value cannot be nil", 3)
 	end
 
-	local value: T = initial
+	local value: T = initial 
 	local callbacks: { [number]: (old: T, new: T) -> () } = {}
 
 	local self = {} :: typings.state<T>
 
+	-- https://en.wikipedia.org/wiki/Event_(computing)
+	-- https://en.wikipedia.org/wiki/Observer_pattern
 	function self.respond(_: any, fn: (old: T, new: T) -> ()): () -> ()
 		if typeof(fn) ~= "function" then
 			errors.new("state", "respond callback must be a function", 3)
@@ -34,15 +36,17 @@ function state.new<T>(initial: T): typings.state<T>
 
 	setmetatable(self, {
 		__call = function(_: any, newValue: T?, forceUpdate: boolean?): T
-			if newValue ~= nil then
-				local shouldUpdate = newValue ~= value or forceUpdate == true
-
+			if newValue ~= nil then 
+				-- this is how the distinctUntilChanged is implemented - check if the oldValue is equal to the newValue, or get overridden by forceUpdate.
+				-- https://www.learnrxjs.io/learn-rxjs/operators/filtering/distinctuntilchanged
+				local shouldUpdate = newValue ~= value or forceUpdate == true 
+				
 				if shouldUpdate then
 					local old = value
 					value = newValue
-
+										
 					for _, fn in callbacks do
-						local ok, err = pcall(fn, old, newValue)
+						local ok, err = pcall(fn, old, newValue) -- https://create.roblox.com/docs/reference/engine/globals/LuaGlobals#pcall
 						if not ok then
 							errors.new("state", `callback error: {err}`, 2)
 						end
@@ -58,6 +62,8 @@ function state.new<T>(initial: T): typings.state<T>
 end
 
 setmetatable(state, {
+	-- to allow calling a table function, we modify the metatable's __call method - see more below
+	-- https://devforum.roblox.com/t/all-you-need-to-know-about-metatables-and-metamethods/503259
 	__call = function<T>(_: typeof(state), value: T): typings.state<T>
 		return state.new(value)
 	end,
